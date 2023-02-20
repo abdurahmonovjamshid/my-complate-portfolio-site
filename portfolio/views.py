11f48b550f231c01c1c37cbdd9868cdef1f2fb7e
unknown <jamshidabdurahmonov060@gmail.com>
2023-02-20 13:07:49 +0500

    cv download

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.http import FileResponse
 
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+
+def download(request):
+    img = open('files/Abdurahmonov Jamshid.doc', 'rb')
+
+    response = FileResponse(img)
+
+    return response
